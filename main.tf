c4c210ff9887759a6bc2c33b63fc220f92f96891
MikhailPasechnik <michail.goodchild@gmail.com>
2021-04-19 06:34:56 +0300

    wp WIP

--- main.tf
+++ main.tf
@@ -29,8 +29,8 @@ module "mysql" {
   source = "./modules/mysql"
 }
 
-# module "wordpress" {
-#   source = "./modules/wordpress"
-#   bucket_name = module.cdn-bucket.bucket_name
-#   bucket_sa_credentials_path = var.bucket_sa_credentials_path
-# }
+module "wordpress" {
+  source = "./modules/wordpress"
+  bucket_name = module.cdn-bucket.bucket_name
+  bucket_sa_credentials_path = var.bucket_sa_credentials_path
+}
